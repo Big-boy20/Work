99e197ab08371fb9df573adee63ae196918ac4b5
Dima <dimon18112000@gmail.com>
2021-07-03 17:35:33 +0300

    firs version ua

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Dashboard from './components/Heder.js'
 
 const App = () => {
   return (
+    
     <div>
+      <Dashboard/>
      <p>Hello world!</p>
-      <p>Hello world(by Dima)!</p>
+      <p>Hello world(by Dimapppp)!</p>
     </div>
   )
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
+
