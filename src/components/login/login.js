67e245ac2024d9cb6e55f4cbbce3046c1fa28df3
Dima <dimon18112000@gmail.com>
2021-07-04 18:38:11 +0300

    New login page

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,21 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
+import './../css/main.css'
 
 export default function Login () {
     
   return (
+    <div className="container">
     <div className="login__container">
     <form>
-         <h2 className="login__text">Email</h2>
-         <input name="email" type="email" placeholder="Email" />
+         <h2 className="login__text">Username</h2>
+         <input className="dataIn" name="email" type="email" placeholder="Email" />
     
     
          <h2 className="login__text">Password </h2> 
-         <input name="password" type="password" placeholder="Password" />
+         <input className="dataIn" name="password" type="password" placeholder="Password" />
   
    </form>
-   <button type="submit" className="button__login">Log-in</button >
+   <button type="submit" className="button__login">Sign up</button >
+    </div>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import Login from './components/login/login.js'
-import './components/css/main.css'
+
 
 const App = () => {
   return (
